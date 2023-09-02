4b232f601b754f63fa2df3b4c06fcbf967ad82f7
sairohitp <sairohitp007@gmail.com>
2023-09-02 23:14:59 +0530

    finalversioning

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+inputBox.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        addTask();
+    }
+});
+
 function addTask() {
     if (inputBox.value == "") {
         alert("You must write something.");
@@ -8,23 +14,27 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        let span = document.createElement("span")
-        span.innerHTML = "\u00d7"
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
     inputBox.value = "";
     saveData();
 }
 
-listContainer.addEventListener("click", function(e) {
-    if (e.target.tagName == "LI") {
-        e.target.classList.toggle("checked")
-        saveData();
-    } else if (e.target.tagName == "SPAN") {
-        e.target.parentElement.remove()
-        saveData();
-    }
-}, false);
+listContainer.addEventListener(
+    "click",
+    function (e) {
+        if (e.target.tagName == "LI") {
+            e.target.classList.toggle("checked");
+            saveData();
+        } else if (e.target.tagName == "SPAN") {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    },
+    false
+);
 
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
